2898f433e357fe3cedff882f524c0033cc57128e
faharetana-rkt <rakotomaharitra@gmail.com>
2024-10-09 11:10:03 +0300

    finished delete, update, & create contact. Finished swagger

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./swagger'));
+
 router.get('/', (req, res) => {
+    //#swagger.tags=['Hello World']
     res.send('Hello World');
 });
 
--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', require('./routes')); 
 
